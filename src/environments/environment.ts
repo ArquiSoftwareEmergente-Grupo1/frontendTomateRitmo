ed7f2f4fbb202d6a1e2052f4713c63510cb4f957
P.G. <129306198+GrandProgTest@users.noreply.github.com>
2025-07-10 00:06:48 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
  export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://tomateritmowebapp-g7dhecbme6hjg6fm.canadacentral-01.azurewebsites.net/api/v1',
   iaUrl: 'https://ab932b44cad2.ngrok-free.app'
 };
