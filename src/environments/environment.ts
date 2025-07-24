cedac3182034eaceba6795f866017b7a7f6eeef2
EliasYP <133830443+EliasYP@users.noreply.github.com>
2025-07-23 21:56:07 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
  export const environment = {
   production: true,
   apiUrl: 'https://tomateritmowebapp-g7dhecbme6hjg6fm.canadacentral-01.azurewebsites.net/api/v1',
-  iaUrl: 'https://ab932b44cad2.ngrok-free.app'
+  iaUrl: 'https://d37c9510ae85.ngrok-free.app'
 };
